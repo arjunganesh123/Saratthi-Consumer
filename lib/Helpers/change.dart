eda9caecb8716aa315c59d7fe00b905c14ee9f92
arjunganesh123 <arjunganeshgonibeedu44@gmail.com>
2022-06-14 02:00:28 +0530

    My first commit with completion of my first newuser screen

---
+++ lib/Helpers/change.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+
+class Change{
+  String image;
+  double width;
+  Color changecolor;
+  Change({required this.image,required this.width,required this.changecolor});
+}
+
+class Change2{
+  String image;
+  Color changecolor;
+  Change2({required this.image,required this.changecolor});
+}
