c458e4de045c0076fe4b4884bd29f0badaf1c7be
arjunganesh123 <arjunganeshgonibeedu44@gmail.com>
2022-06-21 18:56:34 +0530

    last commit of frontend

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:saratthi_consumer/Views/Login/driverpage.dart';
+import 'package:saratthi_consumer/Views/Login/drivesdetails.dart';
+import 'package:saratthi_consumer/Views/Login/edit%20user.dart';
 import 'Views/Login/splash.dart';
 
 void main() async {
@@ -30,7 +33,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: splash(),
+      home: ProfileForm(),
     );
   }
 }
